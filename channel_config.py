2d36f8382b6498c126c01cb996d8723480b19d15
lwhite718 <admin@ennovativedigital.com>
2025-08-24 00:29:07 -0400

    Create channel_config.py

---
+++ channel_config.py
@@ -0,0 +1,77 @@
+channel_scores = {
+    "Google Ads": {
+        "base_scores": {
+            "Awareness": 3,
+            "Engagement": 1,
+            "Lead Gen": 5,
+            "Sales": 5
+        },
+        "modifiers": {
+            "B2B": 1,
+            "B2C": 0,
+            "TOFU": 0,
+            "MOFU": 1,
+            "BOFU": 2
+        }
+    },
+    "Meta Ads": {
+        "base_scores": {
+            "Awareness": 5,
+            "Engagement": 5,
+            "Lead Gen": 4,
+            "Sales": 3
+        },
+        "modifiers": {
+            "B2B": -1,
+            "B2C": 1,
+            "TOFU": 1,
+            "MOFU": 1,
+            "BOFU": 1
+        }
+    },
+    "LinkedIn Ads": {
+        "base_scores": {
+            "Awareness": 2,
+            "Engagement": 2,
+            "Lead Gen": 5,
+            "Sales": 4
+        },
+        "modifiers": {
+            "B2B": 2,
+            "B2C": -1,
+            "TOFU": 0,
+            "MOFU": 1,
+            "BOFU": 1
+        }
+    },
+    "Reddit Ads": {
+        "base_scores": {
+            "Awareness": 4,
+            "Engagement": 4,
+            "Lead Gen": 3,
+            "Sales": 2
+        },
+        "modifiers": {
+            "B2B": 0,
+            "B2C": 1,
+            "TOFU": 1,
+            "MOFU": 0,
+            "BOFU": -1
+        }
+    },
+    "TikTok Ads": {
+        "base_scores": {
+            "Awareness": 5,
+            "Engagement": 5,
+            "Lead Gen": 2,
+            "Sales": 1
+        },
+        "modifiers": {
+            "B2B": -2,
+            "B2C": 2,
+            "TOFU": 2,
+            "MOFU": 0,
+            "BOFU": -2
+        }
+    }
+}
